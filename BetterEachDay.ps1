fee68c549ee85d2ced3ceaf15e9124ef209e6cca
Doug Ericson <dgericson@gmail.com>
2021-10-07 11:04:42 -0400

    dougernaut-powershell

--- BetterEachDay.ps1
+++ BetterEachDay.ps1
@@ -1,12 +1,22 @@
 ######################
+# Accept arguments
+###################### 
+param ($startDate, $increment, $activity)
+
+######################
 #Setting the variables
 ######################
 $currentDay = Get-Date
 #For now we are using 1 Jan of the current year as the start date.
 #TODO: Add ability for user input of start date
 $firstDay = "01/01/"+$currentDay.Year
+if ($activity -eq $null) {    
+    $activity = "your activity"
+}
 
 # Calculate the number of days since the first day
-$days = ($currentDay-(get-date $firstDay))
+$datesDifference = ($currentDay-(get-date $firstDay))
+$days = $datesDifference.Days
 #print results to the screen
-"Today is day " + $days.Days + " of the current year"
+write-host "Today is day $days of the current year."
+write-host "You should do $days reps of $activity today!"
